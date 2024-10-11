b512cdd65addb8da9ddae923d4046dd4ed687678
AngeKri <10511638@ucn.dk>
2024-10-11 22:56:02 +0200

    update

--- FrameworkRubric/src/App.vue
+++ FrameworkRubric/src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
+import {ref} from "vue";
+
 
 </script>
 
 <template>
+<h1>Klimaforandringernes konsekvenser for naturen</h1>
+<p>Klimaforandringerne påvirker alle områder af verden. Polarisen smelter, og vandstanden i havene stiger. I nogle områder bliver ekstreme vejrfænomener og nedbørsmængder mere almindelige, mens man i andre regioner oplever mere ekstreme hedebølger og tørker. Det er nødvendigt med en klimaindsats nu, ellers vil disse virkninger kun blive værre.</p>
+<div></div>
   
 </template>
 
