4822faf7d5c0f71921be0c515884959d247b245e
AngeKri <10511638@ucn.dk>
2024-10-13 13:04:31 +0200

    Update

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <h1>KLIMAKRISE</h1>
+        <span class="material-symbols-outlined">menu</span>
+    </header>
+   
+</template>
+
+<style scoped>
+header {
+  padding: 1rem 0;
+  background: #bb8716;
+  color: white;
+  font-size: 1rem;
+  margin: 0;
+}
+
+h1{
+    margin: auto;
+    width: 45%;
+    font-family: Arial, Helvetica, sans-serif;
+}
+</style>
